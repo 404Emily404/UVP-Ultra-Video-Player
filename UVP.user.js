621e9e4afa1c567181867d6b07287231d273d0c7
404Emily404 <131435776+404Emily404@users.noreply.github.com>
2026-09-05 02:27:25 -0600

    Add UVP.user.js (starter placeholder)

---
+++ UVP.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name        UVP Ultra Video Player
+// @version     5.0.1
+// @description Universal Native Overlay Player • Stream Grabber • In-Browser Remuxing Engine
+// @author      Dragon.Magic (404Emily404)
+// @match       *://*/*
+// @grant       none
+// @run-at      document-idle
+// ==/UserScript==
+
+(function () {
+  'use strict';
+
+  // UVP starter file. Paste your userscript code below.
+
+  console.log('UVP user script placeholder (v5.0.1)');
+
+  // Paste code here.
+
+})();
