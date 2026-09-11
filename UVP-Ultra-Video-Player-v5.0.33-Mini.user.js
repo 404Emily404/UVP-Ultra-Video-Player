006a729052fed8252de28750bb49b2d2c43df75c
404Emily404 <131435776+404Emily404@users.noreply.github.com>
2026-09-11 04:07:24 -0600

    Delete UVP-Ultra-Video-Player-v5.0.33-0Mini.user.js
    Ai fucked it up because it couldnt follow instruction to toggle of debug true to false.

--- UVP-Ultra-Video-Player-v5.0.33-Mini.user.js
+++
@@ -1,7 +0,0 @@
-  const UVP = {}; const state = {}; let DEBUG = false; // DEBUG now OFF by default
-  const CONFIG = {}; const ytPlayerData = {}; const ytRouteGeneration = 0; const ytFmtMap = new Map();
-  const ytDecipheredUrls = new Set(); const capturedUrls = new Set();
-  const _videoCache = { list: null, at: 0 }; let _urlCache = null; let _inlineScripts = null;
-  const lastFragmentFetch = {}; const _hostSlots = new Map(); let _pendingDbClear = null;
-  const STORE = 'chunks'; const DB_NAME = 'uvp-dl'; const ACTIVE_JOB_PREFIX = 'uvp-job:';
-  const ytGateLog = []; let dbPromise = null; const PREFETCH = 3;
